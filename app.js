9c9326e3adcffe7783a5396e72ec3cfb07812333
Nicole <nicole.silfverling@gmail.com>
2022-03-02 17:08:18 +0100

    highscore try

--- app.js
+++ app.js
@@ -2,10 +2,14 @@
 
 const cors = require("cors")
 const express = require("express")
+const rug = require('random-username-generator');
 
 const app = express()
 //const PORT = process.env.PORT || 3000
 const PORT = process.env.PORT || 3001
+
+//let users = {}
+let users = [];
 
 app.use('/healthcheck', require('./routes/healthcheck.routes'));
 app.use(express.urlencoded({ extended: true}));
@@ -16,6 +20,38 @@ app.get("/", (req ,res)=>{
     body={"status": "available"}
     res.status(200).send(body)
 })
+
+app.get("/highscores", (req ,res)=>{
+      headers={"http_status":200, "cache-control":  "no-cache"}
+
+      // sortera
+      users.sort((a, b) => b.score - a.score);
+      
+      res.status(200).send(users)
+})
+
+app.get("/registerscore", (req ,res)=>{
+      headers={"http_status":200, "cache-control":  "no-cache"}
+      let user = req.query.user
+      let score = req.query.score
+      
+      let data = {"user": user, "score": score}
+      if(users.length <= 5){
+            users.push(data);
+      } else if(score > users[4].score){
+            users.splice(4, 1);
+            users.push(data);
+      }
+      
+      res.status(200).send({"status":"success"})
+
+})
+
+app.get('/auth', (req, res) => {
+      let user = rug.generate();
+      users[user] = 0
+      res.status(200).send({"user":user})
+});
 
 app.get("/football", (req ,res)=>{
     headers={"http_status":200, "cache-control":  "no-cache"}
@@ -79,6 +115,8 @@ app.get("/football", (req ,res)=>{
       res.set('Content-Type', 'application/json');
       res.status(200).send(body)
    })
+
+
 
 
 app.listen(PORT , ()=>{
